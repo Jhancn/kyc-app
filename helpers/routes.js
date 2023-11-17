266c07a08cbbe33ceb0b4cab7f577a8cd91a1420
Jhancn <jhancn39@gmail.com>
2023-11-17 13:43:22 +0530

    route to check

--- helpers/routes.js
+++ helpers/routes.js
@@ -4,6 +4,7 @@ const user = require('../routes/user')
 
 module.exports = (app) => {
     app.get('/', (req, res) => res.send('Welcome to kyc!'))
+    app.get('/check', (req, res) => res.send('check done!'))
     app.use('/api/kyc', kyc, queue({ activelimit: 1, queuedlimit: -1 }))
     app.use('/api/user', user, queue({ activelimit: 1, queuedlimit: -1 }))
 }
